348306d8467c8525adcdf212f85ef2b2d6930bac
zae._.hyeong <p0138@naver.com>
2022-01-16 22:33:10 +0900

    change View to Recycler View in TripcourseActivity

--- app/src/main/java/com/olympos/tripbook/src/tripcourse/TripcourseActivity.kt
+++ app/src/main/java/com/olympos/tripbook/src/tripcourse/TripcourseActivity.kt
@@ -1,4 +1,23 @@
 package com.olympos.tripbook.src.tripcourse
 
-class TripcourseActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.olympos.tripbook.databinding.ActivityTripcourseBinding
+
+class TripcourseActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityTripcourseBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTripcourseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initView();
+    }
+
+    private fun initView() {
+
+    }
 }
--- app/src/main/java/com/olympos/tripbook/src/tripcourse/TripcourseRecordActivity.kt
+++ app/src/main/java/com/olympos/tripbook/src/tripcourse/TripcourseRecordActivity.kt
@@ -1,4 +1,21 @@
 package com.olympos.tripbook.src.tripcourse
 
-class TripcourseRecordActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.olympos.tripbook.databinding.ActivityTripcourseRecordBinding
+
+class TripcourseRecordActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityTripcourseRecordBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTripcourseRecordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+
 }
