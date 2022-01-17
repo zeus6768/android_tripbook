8770dd4b2f5e78e4f82dc2ad4a15ae80efa9ca49
zae._.hyeong <p0138@naver.com>
2022-01-17 21:56:50 +0900

    tripcouse - delete Recycler view and make item_album

--- app/src/main/java/com/olympos/tripbook/src/tripcourse/TripcourseActivity.kt
+++ app/src/main/java/com/olympos/tripbook/src/tripcourse/TripcourseActivity.kt
@@ -2,12 +2,17 @@ package com.olympos.tripbook.src.tripcourse
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.olympos.tripbook.R
+import com.olympos.tripbook.config.BaseActivity
 import com.olympos.tripbook.databinding.ActivityTripcourseBinding
 
-class TripcourseActivity : AppCompatActivity() {
+class TripcourseActivity : BaseActivity() {
 
     lateinit var binding : ActivityTripcourseBinding
+
+    val albums = ArrayList<View>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/olympos/tripbook/src/tripcourse/TripcourseRecordActivity.kt
+++ app/src/main/java/com/olympos/tripbook/src/tripcourse/TripcourseRecordActivity.kt
@@ -3,9 +3,10 @@ package com.olympos.tripbook.src.tripcourse
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.olympos.tripbook.config.BaseActivity
 import com.olympos.tripbook.databinding.ActivityTripcourseRecordBinding
 
-class TripcourseRecordActivity : AppCompatActivity() {
+class TripcourseRecordActivity : BaseActivity() {
 
     lateinit var binding : ActivityTripcourseRecordBinding
 
