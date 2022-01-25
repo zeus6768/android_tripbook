bb999dccd28cbc3ce4abbb4d66a65ef40454c525
Zeus6768 <akaps6768@gmail.com>
2022-01-25 21:13:40 +0900

    Delete git cache files and Apply .gitignore

--- app/src/main/java/com/olympos/tripbook/src/user/SigninActivity.kt
+++ app/src/main/java/com/olympos/tripbook/src/user/SigninActivity.kt
@@ -1,9 +1,13 @@
 package com.olympos.tripbook.src.user
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import com.olympos.tripbook.config.BaseActivity
 import com.olympos.tripbook.databinding.ActivityUserSigninBinding
+import com.olympos.tripbook.src.home.MainActivity
 import com.olympos.tripbook.src.user.model.User
+import retrofit2.Retrofit
 
 class SigninActivity : BaseActivity() {
     private lateinit var binding: ActivityUserSigninBinding
@@ -12,12 +16,14 @@ class SigninActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityUserSigninBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.signinSigninBtnIv.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
-    private fun getUser(): User {
-        //val email: String = binding.signupEdittext.text.toString() + "@" + binding.signupEmailEdittext.text.toString()
-        val email : String = binding.signinEmailEdittext.text.toString()
-        val pwd: String = binding.signinPasswordEdittext.text.toString()
-        return User(email, pwd, "", "")
+    private fun kakaoSignin() {
+        val retrofit = Retrofit.Builder().baseUrl()
     }
 }
