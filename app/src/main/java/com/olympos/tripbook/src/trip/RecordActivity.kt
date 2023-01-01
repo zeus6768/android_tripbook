aab86673433dd41a91d0274327bb5c1b6f14ab86
nayooniverse <kangna99@naver.com>
2023-01-01 23:52:12 +0900

    commit before refactoring

--- app/src/main/java/com/olympos/tripbook/src/trip/RecordActivity.kt
+++ app/src/main/java/com/olympos/tripbook/src/trip/RecordActivity.kt
@@ -11,8 +11,9 @@ import com.olympos.tripbook.config.BaseActivity
 import com.olympos.tripbook.databinding.ActivityRecordBinding
 import com.olympos.tripbook.src.tripcourse.model.TripCourse
 
-class RecordActivity : BaseActivity() {
+class RecordActivity : BaseActivity(), DateSelectDialog.DialogClickListener {
     private lateinit var binding: ActivityRecordBinding
+    lateinit var tripDate: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,6 +64,10 @@ class RecordActivity : BaseActivity() {
 
         //click 리스너
         binding.recordTopbarLayout.topbarBackIb.setOnClickListener(this)
+        binding.recordTopbarLayout.topbarSubbuttonIb.setOnClickListener(this)
+        binding.recordSelectDateBtn.setOnClickListener(this)
+        binding.recordSelectCountryBtn.setOnClickListener(this)
+        binding.recordHashtagAddBtn.setOnClickListener(this)
     }
 
     private fun setCourse(course: TripCourse) {
@@ -79,6 +84,16 @@ class RecordActivity : BaseActivity() {
                 showDialog("발자국 작성 취소", "발자국 작성을 취소하시겠습니까?\n" + "작성하셨던 내용은 사라집니다.", "확인")
             R.id.topbar_subbutton_ib -> {
                 //todo 입력받은 정보 저장
+                saveInfo()
+            }
+            R.id.record_select_date_btn -> {
+                showDateDialog("여행 날짜 선택", "날짜 선택 완료")
+            }
+            R.id.record_select_country_btn -> {
+
+            }
+            R.id.record_hashtag_add_btn -> {
+
             }
         }
     }
@@ -87,5 +102,25 @@ class RecordActivity : BaseActivity() {
     override fun onOKClicked() {
         super.onOKClicked()
         finish()
+    }
+
+    private fun saveInfo() {
+        //정보 저장 후 나가기
+        finish()
+    }
+
+    private fun showDateDialog(title: String, okMessage: String) {
+        val dig = DateSelectDialog(this)
+        dig.listener = this
+        dig.show(title, okMessage)
+    }
+
+    override fun onDateOKClicked(selectedYear: Int, selectedMonth: Int, selectedDay: Int) {
+        binding.recordSelectDateBtn.text = String.format("%d년 %d월 %d일", selectedYear, selectedMonth, selectedDay)
+        tripDate = String.format("%d-%d-%d", selectedYear, selectedMonth, selectedDay)
+    }
+
+    override fun onDateCancelClicked() {
+        TODO("Not yet implemented")
     }
 }
